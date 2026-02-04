a6ff38f7e7b48b0c64c9ea1e6001f19d810e6dee
unknown <niloynag19@gmail.com>
2026-02-04 23:40:04 +0600

    implement the dashboard layout section

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,12 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
-
-
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session = authClient.getSession();
+
+  const cookieStore = await cookies();
+  console.log(cookieStore.toString());
+
+  const res = await fetch("http://localhost:5000/api/auth/get-session",{
+    headers:{
+      Cookie:cookieStore.toString(),
+    },
+    cache:"no-store"
+  });
+
+  const session = await res.json();
+
   console.log(session);
-  
   return (
     <Button/>
   );
