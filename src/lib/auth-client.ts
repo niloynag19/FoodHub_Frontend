ab0d9f9593a20651a98faccff4deb48ce67efd7e
unknown <niloynag19@gmail.com>
2026-05-13 11:30:19 +0600

    Restore window.location.origin in auth-client.ts to fix login

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
     // Use the full URL for Server Components, and relative for the Browser
-    baseURL: "https://foodhub-server-chi.vercel.app",
+    baseURL: typeof window === "undefined" ?
+        "https://foodhub-server-chi.vercel.app" : window.location.origin,
     
     fetchOptions: {
         credentials: "include",
